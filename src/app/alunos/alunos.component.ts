cd57a9cf7ddd7f63c68616fc8ab6e2b5a391ddf3
Gustavo Luchetti <gusta.luchetti@gmail.com>
2020-04-06 18:54:38 -0300

    added very basic grid

--- src/app/alunos/alunos.component.ts
+++ src/app/alunos/alunos.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-alunos',
   templateUrl: './alunos.component.html',
   styleUrls: ['../app.component.css']
 })
-export class AlunosComponent implements OnInit {
+export class AlunosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    
   }
-
+  
+  
+  alunos = [
+    {
+      idAluno: 1,
+      idCurso: 'Judo',
+      nomeAluno: 'Peter Parker'
+    }
+  ];
+  
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',

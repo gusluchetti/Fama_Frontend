a6be72493d9385f0f25a7ea64da1b6bb2850ea10
Gustavo Luchetti <gusta.luchetti@gmail.com>
2020-05-18 19:01:29 -0300

    implementacao basica do CRUD nas areas de cursos e funcionarios; concretizacao do style das telas

--- src/app/funcionarios/funcionarios.component.ts
+++ src/app/funcionarios/funcionarios.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FuncionarioModel } from '../models/models';
+import { FuncionarioService } from './funcionarios.service';
 
 @Component({
   selector: 'app-funcionarios',
@@ -7,21 +8,73 @@ import { FuncionarioModel } from '../models/models';
   styleUrls: ['../app.component.css']
 })
 export class FuncionariosComponent{
-// funcionarios: FuncionarioModel [] = [];
+  selectedFuncionario: FuncionarioModel = null
+  emptyFuncionario = {} as FuncionarioModel
+  listFuncionarios = [] as FuncionarioModel[]
 
-  constructor() { }
+  editing: boolean = false
+  funcionarioExists: boolean = false
 
-  funcionarios = [
-    {
-      idFuncionario: 1,
-      idCurso: 'Karate',
-      nomeFuncionario: 'Karate Man'
-    },
-    {
-      idFuncionario: 2,
-      idCurso: 'Arco e Flecha',
-      nomeFuncionario: 'Gaviao Arqueiro'
-    },
-  ];
 
+  ngOnInit() { 
+    this.service.listar().subscribe((data: any) => {
+      this.listFuncionarios = data;
+    });
+  }
+
+  constructor(
+    private service: FuncionarioService) { 
+
+    }
+
+  changeMode() { this.editing = !this.editing }
+
+  onEdit(c?: FuncionarioModel) {
+    if(c != null || c!= undefined) {
+      this.selectedFuncionario = c
+      this.funcionarioExists = true
+    }
+    this.changeMode()
+  }
+
+  onRemove(c: FuncionarioModel) {
+    this.service.excluir(c.idFuncionario).subscribe();
+    let index = this.listFuncionarios.indexOf(c)
+
+    if(index != null || index != undefined) {
+      this.listFuncionarios.splice(index, 1)
+    }
+  }
+
+  onCancel() {
+    this.selectedFuncionario = {} as FuncionarioModel
+    this.changeMode()
+  }
+
+  //!!!!!TODO: ONLY CHANGE LISTS IF SUCESS
+  onFinish() {
+    console.log("funcionario a ser enviado", this.selectedFuncionario)
+    // alterar funcionario existente, else adicionar novo funcionario
+     if(this.funcionarioExists) {
+      console.log("UPDATE")   
+      this.service.alterar(this.selectedFuncionario).subscribe((data: any) => {});
+      let success = true;
+
+      if (success) {
+        console.log("Funcionario alterado com sucesso!")
+      }
+    }
+     else {
+      console.log("CREATE")   
+      this.service.criar(this.selectedFuncionario).subscribe((data: any) => {});
+      let success = true;
+
+      if (success) {
+        this.listFuncionarios.push(this.selectedFuncionario)
+        console.log("Funcionario criado com sucesso!")
+
+      }
+    }
+    this.changeMode()
+  }
 }
---
+++ src/app/funcionarios/funcionarios.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, NgModule } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { ActivatedRoute } from '@angular/router';
+import { FuncionarioModel } from '../models/models';
+
+@NgModule()
+export class FuncionarioService {
+
+    private subscriber: any;
+    lista: any;
+
+    httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': 'my-auth-token'
+        })
+      };
+
+    constructor(
+        private http: HttpClient,
+        private route: ActivatedRoute
+    ) { }
+
+    criar(funcionario: FuncionarioModel) {
+        return this.http.post('/api/funcionario/criar', JSON.stringify(funcionario), this.httpOptions);
+    }
+
+    listar() {
+        return this.http.get('/api/funcionario/listar');
+    }
+
+    alterar(funcionario: FuncionarioModel) {
+        return this.http.post('/api/funcionario/alterar', JSON.stringify(funcionario), this.httpOptions);
+    }
+
+    excluir(idFuncionario: number) {
+        return this.http.get('/api/funcionario/excluir/?idFuncionario='+ idFuncionario, this.httpOptions);
+    }
+    
+}

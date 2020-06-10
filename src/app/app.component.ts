c14800c00a1d428bda80aaade2424c3e35004a58
Gustavo Luchetti <gusta.luchetti@gmail.com>
2020-06-10 17:40:13 -0300

    aviso para selecionar aba na pagina inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component, NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
+	router: string;
 
-} 
+	constructor(public _router: Router) {}
+	ngOnInit() {
+		console.log(this.router)
+	}
+
+}
+
